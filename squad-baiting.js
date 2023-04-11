0a2fb656aa135bff4aa850f3cb0c00150de2a1df
Davide Fantino <80767709+fantinodavide@users.noreply.github.com>
2023-04-11 02:30:24 +0200

    fix: player list

--- squad-baiting.js
+++ squad-baiting.js
@@ -34,6 +34,7 @@ export default class SquadBaiting extends DiscordBasePlugin {
         // this.discordLog = this.discordLog.bind(this)
 
         this.playerBaiting = new Map();
+        this.squadsBaiting = new Map();
 
         this.broadcast = (msg) => { this.server.rcon.broadcast(msg); };
         this.warn = (steamid, msg) => { this.server.rcon.warn(steamid, msg); };
@@ -44,15 +45,26 @@ export default class SquadBaiting extends DiscordBasePlugin {
         let oldSquads = [];
 
         setInterval(async () => {
-            const newSquads = this.server.squads.map(e => ({ ...e, leader: this.server.players.find(p => p.squadID == e.squadID && p.teamID == e.teamID) }));
+            await this.server.updateSquadList();
+            // const squads = this.verbose(1, '', (await this.server.rcon.execute('ListSquads')).split('\n').map(e => /^ID:\s*(?<squadID>\d+)\s*\|\s*Name:\s*(?<squadName>[^|\s].*?)\s*\|\s*Size:\s*(?<size>\d+)\s*\|\s*Locked:\s*(?<locked>True|False)\s*\|\s*Creator Name:\s*(?<creator_name>[^|\s].*?)\s*\|\s*Creator Steam ID:\s*(?<creator_steam_id>\d+)$/i.exec(e)?.groups).filter(e => e != null));
+            const players = (await this.server.rcon.execute('ListPlayers')).split('\n').map(e => /^ID: (?<id>\d+)\s+\|\s+SteamID: (?<steamID>\d+)\s+\|\s+Name: (?<name>[^|]+)\s+\|\s+Team ID: (?<teamID>\d)\s+\|\s+Squad ID: (?<squadID>\d)\s+\|\s+Is Leader: (?<isLeader>True|False)\s+\|\s+Role: (?<role>.+)$/.exec(e)?.groups).filter(e => e != null);
+            // this.verbose(1, '', players)
+
+            const newSquads = this.server.squads.map(e => ({ ...e, leader: players.find(p => p.squadID == e.squadID && p.teamID == e.teamID && p.isLeader == 'True') }));
             oldSquads.forEach(async s => {
                 const match = newSquads.find(ns => ns.squadID == s.squadID && ns.teamID == s.teamID && ns.squadName == s.squadName)
                 const baiting = match && match.leader.steamID != s.leader.steamID;
+                const sqUid = `${s.teamID};${s.squadID};${s.squadName}`;
+                this.verbose(1, 'baiting', sqUid, s.leader.name)
                 if (baiting) {
                     const plBaitingAmount = (this.playerBaiting.get(s.leader.steamID) || 0) + 1;
                     this.playerBaiting.set(s.leader.steamID, plBaitingAmount)
+
+                    const sqBaitsAmount = (this.squadsBaiting.get(sqUid) || 0) + 1;
+                    this.squadsBaiting.set(sqUid, sqBaitsAmount)
+
                     await this.warn(s.leader.steamID, 'Squad baiting is not allowed!')
-                    await this.warnAdmins(`[${s.leader.name}] is doing squad baiting. ${plBaitingAmount} times.`)
+                    await this.warnAdmins(`[${s.leader.name}] is doing squad baiting.\n  Player's baits: ${plBaitingAmount}\n  Squad Info:\n   Name: ${s.squadName}\n   Number: ${s.squadID}\n   Team: ${s.teamID}\n   Baits: ${sqBaitsAmount}`)
                 }
             })
 
